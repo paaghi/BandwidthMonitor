da5ca9d7a557ccbf1a458e339744255955ba8d9a
Pooya Afaghi <paaghi@gmail.com>
2016-01-31 18:14:08 -0400

    First commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace BandwidthMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cmbInterface = new System.Windows.Forms.ComboBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.contextMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbInterface
+            // 
+            this.cmbInterface.FormattingEnabled = true;
+            this.cmbInterface.Location = new System.Drawing.Point(5, 5);
+            this.cmbInterface.Name = "cmbInterface";
+            this.cmbInterface.Size = new System.Drawing.Size(121, 21);
+            this.cmbInterface.TabIndex = 0;
+            this.cmbInterface.SelectionChangeCommitted += new System.EventHandler(this.cmbInterface_SelectionChangeCommitted);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(130, 9);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(35, 13);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "label1";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(184, 8);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(35, 13);
+            this.lblSpeed.TabIndex = 3;
+            this.lblSpeed.Text = "label2";
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(12, 42);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(397, 168);
+            this.chart.TabIndex = 4;
+            this.chart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 222);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.cmbInterface);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbInterface;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices; //DLLImport
+using System.IO; //Path.Combine
+using Ini; //Add "Ini.cs"
+
+namespace BandwidthMonitor
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
+
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceFrequency(out long lpFrequency);
+
+        private NetworkInterface[] nicArr;
+        private NetworkInterface nic;
+        private double ElapsedTime, BytesSent = 0, BytesReceived = 0;
+        private long Freq, Tic = 0, Toc, Run = -1;
+        private string FN_ini = Path.Combine(Directory.GetCurrentDirectory(), "config.ini");
+        private int chart_x_interval = 20;
+
+        private void mainMenuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateNetworkInterface();
+        }
+
+        private void cmbInterface_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            Run = -1;
+            nic = nicArr[cmbInterface.SelectedIndex];
+            timer.Enabled = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateNetworkInterface()
+        {
+            Run += 1;
+
+            IPv4InterfaceStatistics interfaceStats = nic.GetIPv4Statistics();
+
+            QueryPerformanceCounter(out Toc);
+            ElapsedTime = (double)(Toc - Tic) / Freq;
+            QueryPerformanceCounter(out Tic);
+            double PreviousBytesSent = BytesSent;
+            double PreviousBytesReceived = BytesReceived;
+            BytesSent = interfaceStats.BytesSent;
+            BytesReceived = interfaceStats.BytesReceived;
+
+            if (Run > 0)
+            {
+                int bytesSentSpeed = (int)((double)(BytesSent - PreviousBytesSent) / ElapsedTime / 1024);
+                int bytesReceivedSpeed = (int)((double)(BytesReceived - PreviousBytesReceived) / ElapsedTime / 1024);
+
+                //lblSpeed.Text = "Connection speed = " + (nic.Speed / 1000000).ToString() + " MB/s";
+                //lblInterfaceType.Text = "Interface type = " + nic.NetworkInterfaceType.ToString();
+                lblTotal.Text = "Up/Down traffic: " + ((double)interfaceStats.BytesSent / 1048576).ToString("F3") + " / " + ((double)interfaceStats.BytesReceived / 1048576).ToString("F3") + " MiB";
+                lblSpeed.Text = "Up/Down speed: " + (bytesSentSpeed * 8).ToString() + " / " + (bytesReceivedSpeed * 8).ToString() + " Kibit/s";
+                chart.Series["UpSpeed"].Points.AddXY(Run, bytesSentSpeed * 8);
+                chart.Series["DownSpeed"].Points.AddXY(Run, bytesReceivedSpeed * 8);
+                chart.ChartAreas["ChartArea1"].AxisX.Minimum = Run - chart_x_interval;
+                chart.ChartAreas["ChartArea1"].AxisX.Maximum = Run;
+            }
+            else
+            {
+                chart.Series["UpSpeed"].Points.Clear();
+                chart.Series["DownSpeed"].Points.Clear();
+
+                chart.Series["UpSpeed"].Points.AddXY(0, 0);
+                chart.Series["DownSpeed"].Points.AddXY(0, 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int ini_width = 400, ini_height = 200, ini_interval = 500, ini_chart_max_y = 1200, ini_combo_visible = 1;
+            string ini_default_interface, ini_color_back = "#252526";
+
+            //BackColor = Color.Lime;
+            //TransparencyKey = Color.Lime;
+            //FormBorderStyle = FormBorderStyle.None;
+
+            cmbInterface.DropDownStyle = ComboBoxStyle.DropDownList;
+            nicArr = NetworkInterface.GetAllNetworkInterfaces();
+            for (int i = 0; i < nicArr.Length; i++)
+                cmbInterface.Items.Add(nicArr[i].Name);
+            cmbInterface.SelectedIndex = 0;
+            ini_default_interface = cmbInterface.SelectedText;
+
+            if (File.Exists(FN_ini))
+            {
+                try
+                {
+                    IniFile ini = new IniFile(FN_ini);
+                    ini_width = int.Parse(ini.IniReadValue("settings", "width"));
+                    ini_height = int.Parse(ini.IniReadValue("settings", "height"));
+                    ini_interval = int.Parse(ini.IniReadValue("settings", "interval"));
+                    ini_chart_max_y = int.Parse(ini.IniReadValue("settings", "chart_max_y"));
+                    ini_default_interface = ini.IniReadValue("settings", "default_interface");
+                    ini_combo_visible = int.Parse(ini.IniReadValue("settings", "combo_visible"));
+                    ini_color_back = ini.IniReadValue("settings", "color_back");
+                }
+                catch
+                {
+                    //File.Delete(FN_ini);
+                }
+            }
+
+            if (cmbInterface.FindStringExact(ini_default_interface) != -1)
+                cmbInterface.SelectedIndex = cmbInterface.FindStringExact(ini_default_interface);
+            nic = nicArr[cmbInterface.SelectedIndex];
+
+            if (ini_combo_visible == 0)
+                cmbInterface.Visible = false;
+
+            if (QueryPerformanceFrequency(out Freq) == false)
+                throw new Win32Exception();
+
+            this.Icon = BandwidthMonitor.Properties.Resources.icon; //Add existing "icon.ico" resource
+            notifyIcon.Icon = BandwidthMonitor.Properties.Resources.icon;
+            notifyIcon.Visible = true;
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+
+            this.Size = new Size(ini_width, ini_height);
+            cmbInterface.Size = new Size(120, cmbInterface.Size.Height);
+            cmbInterface.Location = new Point(5, 5);
+            lblTotal.Location = new Point(cmbInterface.Location.X + cmbInterface.Size.Width + 5, 5);
+            lblSpeed.Location = new Point(cmbInterface.Location.X + cmbInterface.Size.Width + 5, 20);
+            chart.Size = new Size(this.Size.Width - 25, this.Size.Height - 85);
+            chart.Location = new Point(5, 40);
+
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = System.Drawing.ColorTranslator.FromHtml(ini_color_back);
+            lblTotal.ForeColor = Color.White;
+            lblSpeed.ForeColor = Color.White;
+            lblTotal.Text = "";
+            lblSpeed.Text = "";
+            cmbInterface.BackColor = this.BackColor;
+            cmbInterface.ForeColor = Color.White;
+            this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width, Screen.PrimaryScreen.WorkingArea.Height - this.Height);
+
+            chart.BackColor = System.Drawing.ColorTranslator.FromHtml(ini_color_back);
+            chart.Series.Clear();
+            chart.Series.Add("UpSpeed");
+            chart.Series.Add("DownSpeed");
+
+            chart.Series["UpSpeed"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart.Series["UpSpeed"].Color = Color.Red;
+            chart.Series["UpSpeed"].BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chart.Series["UpSpeed"].BorderWidth = 2;
+
+            chart.Series["DownSpeed"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart.Series["DownSpeed"].Color = Color.Green;
+            chart.Series["DownSpeed"].BorderWidth = 2;
+
+            chart.Legends["Legend1"].Enabled = false;
+
+            chart.ChartAreas["ChartArea1"].Position.Auto = false;
+            chart.ChartAreas["ChartArea1"].Position.Width = 95;
+            chart.ChartAreas["ChartArea1"].Position.Height = 100;
+            chart.ChartAreas["ChartArea1"].BackColor = System.Drawing.ColorTranslator.FromHtml(ini_color_back);
+
+            chart.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chart.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineColor = Color.Gray;
+            chart.ChartAreas["ChartArea1"].AxisX.MajorTickMark.Enabled = false;
+            chart.ChartAreas["ChartArea1"].AxisX.IsMarginVisible = false;
+            chart.ChartAreas["ChartArea1"].AxisX.LabelStyle.Enabled = false;
+            chart.ChartAreas["ChartArea1"].AxisX.Interval = (int)chart_x_interval / 4;
+            chart.ChartAreas["ChartArea1"].AxisX.LineColor = Color.Gray;
+
+            chart.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chart.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineColor = Color.Gray;
+            chart.ChartAreas["ChartArea1"].AxisY.MajorTickMark.Enabled = false;
+            chart.ChartAreas["ChartArea1"].AxisY.Minimum = 0;
+            chart.ChartAreas["ChartArea1"].AxisY.Maximum = ini_chart_max_y;
+            chart.ChartAreas["ChartArea1"].AxisY.Interval = (int)ini_chart_max_y / 4;
+            chart.ChartAreas["ChartArea1"].AxisY.LabelAutoFitMinFontSize = 7;
+            chart.ChartAreas["ChartArea1"].AxisY.LabelAutoFitMaxFontSize = 7;
+            chart.ChartAreas["ChartArea1"].AxisY.LineColor = Color.Gray;
+            chart.ChartAreas["ChartArea1"].AxisY.LabelStyle.ForeColor = Color.Gray;
+
+            timer.Interval = ini_interval;
+            timer.Enabled = true;
+        }
+    }
+}
